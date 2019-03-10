3c1e9e20cf62d2b6f28906ad2c6bf7d2f8f874c5
Glenn <glenn@GlenndeAir.lan>
2019-03-10 13:32:00 +0800

    project init

---
+++ src/main/java/com/glenn/BTCApplication.java
@@ -0,0 +1,14 @@
+package com.glenn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BTCApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(BTCApplication.class, args);
+    }
+
+}
